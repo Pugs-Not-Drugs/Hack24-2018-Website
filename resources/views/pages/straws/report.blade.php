879bc21cf59328d410802852f91878d5701bc59c
Stephen Brown <stephen@codemakers.co.uk>
2018-03-10 12:59:57 +0000

    updated web config

--- resources/views/pages/straws/report.blade.php
+++ resources/views/pages/straws/report.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 <h2>Report Straws Form</h2>
+
+<form method="POST">
+    {{ csrf_field() }}
+    
+    <p>
+        <a href="/straws">Cancel</a>
+        <button type="submit">Submit Report</button>
+    </p>
+
+</form>
+
 @endsection
 
 @section('js_content')
