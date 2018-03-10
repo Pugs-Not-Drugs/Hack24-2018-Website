902a8ef41396ded9c033c0619080dd1aff749b27
Stephen Brown <stephen@codemakers.co.uk>
2018-03-10 23:32:04 +0000

    http pls

--- app/Libraries/Google/GoogleService.php
+++ app/Libraries/Google/GoogleService.php
@@ -20,7 +20,7 @@ class GoogleService
     public function searchCompanies($keywords) {
   
         $result = file_get_contents(
-            "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.954783%2C-1.158109&radius=5000&type=restaurant&keyword=" . urlencode($keywords) . "&key=AIzaSyAPsnDKSbHd0I13ybmPkYlAfCsvGjvAK_4"
+            "http://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.954783%2C-1.158109&radius=5000&type=restaurant&keyword=" . urlencode($keywords) . "&key=AIzaSyAPsnDKSbHd0I13ybmPkYlAfCsvGjvAK_4"
         );
             
         if(!empty($result)) {
