8e8a44e563ce47c9f1d81be5357fa3f838436a6f
Stephen Brown <stephen@codemakers.co.uk>
2018-03-11 08:16:24 +0000

    guzzle gogoleapi

--- app/Libraries/Google/GoogleService.php
+++ app/Libraries/Google/GoogleService.php
@@ -18,15 +18,15 @@ class GoogleService
     }
 
     public function searchCompanies($keywords) {
-  
-        $result = file_get_contents(
-            "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.954783%2C-1.158109&radius=5000&type=restaurant&keyword=" . urlencode($keywords) . "&key=AIzaSyAPsnDKSbHd0I13ybmPkYlAfCsvGjvAK_4"
-        );
-            
-        if(!empty($result)) {
-            return json_decode($result)->results;
-        }                
-
+        $url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.954783%2C-1.158109&radius=5000&type=restaurant&keyword=" . urlencode($keywords) . "&key=AIzaSyAPsnDKSbHd0I13ybmPkYlAfCsvGjvAK_4";
+        $response = $this->guzzle->get($url);
+        $body = json_decode($response->getBody());
+        
+        if(!empty($body) && !is_object($body)) {
+            return json_decode($body)->results;
+        } else {
+            return $body->results;
+        }
         return [];
             
     }
